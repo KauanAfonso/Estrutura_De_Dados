4e0b4199276238818cac357a5a870b9aa7b823fc
KauanAfonso <kauanafon3@gmail.com>
2025-02-06 22:49:43 -0300

    busca linear

---
+++ busca_linear/index.js
@@ -0,0 +1,14 @@
+const busca_linear =  (elemento, lista)=>{
+
+    for (let i = 0; i < lista.lenght; i++) {
+        if(lista[i] == elemento){
+            return "Elemento encontrado"
+        }
+        
+        return "Elemento não encontrado"
+
+    }
+
+
+
+}
