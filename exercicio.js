66e4e615a6d52c9b0d5f8818e08dc2a24950835d
KauanAfonso <kauanafon3@gmail.com>
2025-02-09 21:43:20 -0300

    desafio

---
+++ exercicio.js
@@ -0,0 +1,23 @@
+function abc(lista1 , lista2){
+
+    let lista_nova = []
+    for (let i = 0; i < lista1.length; i++) {
+         lista_nova.push(lista1[i]);
+        
+    }
+
+    for (let i = 0; i < lista2.length; i++) {
+        lista_nova.push(lista2[i]);
+       
+   }
+
+   lista_nova.sort((a,b) => a-b)
+
+    return lista_nova
+}
+
+
+let a = [1,2,3]
+let b = [5,4,5]
+
+console.log(abc(a,b))
